b59ec04f1c1a4f45c93105e3c564e309e57c72d3
antoine <antoinejulien33@gmail.com>
2016-11-29 07:48:17 +0100

    encore changement href

--- htmlBlocks/connectedHeader.php
+++ htmlBlocks/connectedHeader.php
@@ -1,6 +1,6 @@
 <div style="width:20%" class="center">
-	<a href="http://localhost:8080/pages/account.php"><?php echo $_SESSION['user']->get_user_name() ?></a>
+	<a href="/pages/account.php"><?php echo $_SESSION['user']->get_user_name() ?></a>
 </div>
 <div style="width:19%" class="center">
-	<a href="http://localhost:8080/pages/sign_out.php">Sign-out</a>
+	<a href="/pages/sign_out.php">Sign-out</a>
 </div>
--- htmlBlocks/incognitoHeader.php
+++ htmlBlocks/incognitoHeader.php
@@ -1,13 +1,13 @@
 <div style="width:39%" class="center">
-	<a href="http://localhost:8080/pages/sign_up.php">Sign-up</a>
+	<a href="/pages/sign_up.php">Sign-up</a>
 </div>
 <div style="width:100%" class="center">
-	<form action="http://localhost:8080/pages/validate_sign_in.php" method="post">
+	<form action="/pages/validate_sign_in.php" method="post">
 		<label> Identifiant : </label>
 		<input type="text" name="login">
 		<label> Password : </label>
 		<input type="password" name="pwd">
 		<input type="submit" value="Sign-In">
 	</form>
-	<a href="http://localhost:8080/pages/request_reset_pwd.php">Mot de passe oublié ?</a>
+	<a href="/pages/request_reset_pwd.php">Mot de passe oublié ?</a>
 </div>
