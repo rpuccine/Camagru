9e63e8f72b846b8b3b5a361239d0acddb4677fc5
Remi PUCCINELLI <rpuccine@e1r7p6.42.fr>
2016-12-04 13:15:14 +0100

    modif pagination

--- pages/gallery.php
+++ pages/gallery.php
@@ -1,5 +1,4 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/scripts/tools.php') ?>
-<!-- Optinnal auth Protect -->
 <?php
 	$limit = 5;
 	$nb_montage = Montage::get_nb_montage();
@@ -15,42 +14,27 @@
 	}
 	$offset = $page * $limit - $limit;
 	$montages = Montage::get_montage_paginated($limit, $offset);
-	/* STUFF TO CONTINUE  */
 ?>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/htmlBlocks/head.php') ?>
 
+<!-- Page -->
+<?php include($_SERVER['DOCUMENT_ROOT'].'/htmlBlocks/head.php') ?>
 <body>
 	<?php include($_SERVER['DOCUMENT_ROOT'].'/htmlBlocks/header.php') ?>
-		<div class="page center">
-			<?php if ($page > 1) { ?>
-				<a href='?page=<?php echo ($page - 1) ?>'>prev</a>
-			<?php } ?>
-			<?php for ($i=1; $i <= $last_page; $i++) { ?>
-				<a <?php if ($page == $i) {echo ("class='current'");} ?> href='?page=<?php echo ($i) ?>'><?php echo ($i) ?></a>
-			<?php } ?>
-			<?php if ($page < $last_page) { ?>
-				<a href='?page=<?php echo ($page + 1) ?>'>next</a>
-			<?php } ?>
-		</div>
-		<div class="container center">
-    <!-- get all images -->
+	<!-- Pagination -->
+	<?php include($_SERVER['DOCUMENT_ROOT']
+		.'/htmlBlocks/paginationGallery.php') ?>
+	<!-- get all images -->
+	<div class="container center">
     <?php
       foreach ($montages as $montage ) {
         include($_SERVER['DOCUMENT_ROOT'].'/htmlBlocks/galleryDiv.php');
       }
      ?>
 	</div>
-	<div class="page center">
-		<?php if ($page > 1) { ?>
-			<a href='?page=<?php echo ($page - 1) ?>'>prev</a>
-		<?php } ?>
-		<?php for ($i=1; $i <= $last_page; $i++) { ?>
-			<a <?php if ($page == $i) {echo ("class='current'");} ?> href='?page=<?php echo ($i) ?>'><?php echo ($i) ?></a>
-		<?php } ?>
-		<?php if ($page < $last_page) { ?>
-			<a href='?page=<?php echo ($page + 1) ?>'>next</a>
-		<?php } ?>
-	</div>
+	<!-- Pagination -->
+	<?php include($_SERVER['DOCUMENT_ROOT']
+		.'/htmlBlocks/paginationGallery.php') ?>
+		<!-- Footer -->
 	<?php include($_SERVER['DOCUMENT_ROOT'].'/htmlBlocks/footer.php') ?>
 </body>
 </html>
